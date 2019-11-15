09b0c8872fef78fc2665936f79d2d007f7a8abb2
Danilo Yassuhiko Miyamura <danilo.miyamura@avanade.com>
2019-11-15 03:17:46 -0300

    primeiro commit

---
+++ src/main/java/fatec/petManager/Model/Queue.java
@@ -0,0 +1,13 @@
+package fatec.petManager.Model;
+
+public class Queue {
+	int queuePosition;
+
+	public int getQueuePosition() {
+		return queuePosition;
+	}
+
+	public void setQueuePosition(int queuePosition) {
+		this.queuePosition = queuePosition;
+	}
+}
