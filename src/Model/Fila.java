edf7d26c70e6e68bfccde2385706699c39389958
Jonis Silva Neves <jonis.silva.neves@avanade.com>
2019-11-21 11:56:54 -0300

    Implementado classe fila e atendimento

---
+++ src/Model/Fila.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author jonis.silva.neves
+ */
+public class Fila {
+    
+
+    //Clinica geral - N
+    //Vacinação - N
+    //Fratura - C
+    //Cirurgia - C
+        private int dados[];
+    private int tamanho;
+    
+    public Fila(int capacidade){
+        dados = new int[capacidade];
+        tamanho = 0;
+    }
+    
+    public boolean vazia(){
+        return tamanho == 0;
+        /*
+         * if (tamanho == 0) return true;
+         * 
+         * return false;
+         */
+    }
+    
+    public boolean cheia(){
+        return tamanho == dados.length;
+    }
+
+    public void adicionaFila(int e, String tipo){
+        if(cheia()){
+            System.out.println("Fila cheia");
+        }else{
+            if(tipo == "N"){
+                
+            }else if(tipo == "C"){
+                
+            }
+            dados[tamanho] = e;
+            tamanho++;
+        }
+    }
+    
+  
+    public int removeFila(){
+        int a = dados[0];
+        if(vazia()){
+           System.out.println("Fila Vazia");
+        }else{
+            for(int i = 0; i < tamanho-1; i++){
+                dados[i] = dados[i+1];
+            }
+            tamanho--;
+        }
+     return a;
+   }
+    
+    
+     public String mostra(){
+            String resp = "Elementos:\n";
+            for( int i=0; i <= tamanho-1;i++)
+                resp = resp + "  " + dados[i];
+            return resp;
+     }
+    
+    
+}
